7921ebe0a1d26f3431883a9609f64c191607ced9
Leomara AC <leomara.andrade@gmail.com>
2024-08-20 07:05:30 -0300

    arquivos iniciais

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.google.devtools.ksp)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.runningapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.runningapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+
+    // Material Design 3
+    implementation(libs.androidx.material3)
+
+    //Architectural Components
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Coroutine Lifecycle Scopes
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Navigation Components
+    implementation(libs.androidx.navigation.compose)
+
+    // Glide
+    implementation(libs.github.bumptech.glide.compose)
+
+    // Google Maps Location Services
+    implementation(libs.google.android.gms.play.service.location)
+    implementation(libs.google.android.gms.play.service.maps)
+    implementation(libs.google.android.maps.compose)
+    implementation(libs.google.android.maps.ktx)
+    implementation(libs.google.android.maps.utils.ktx)
+
+    // Easy Permissions
+    implementation(libs.pub.devrel.easypermissions)
+
+    // Timber
+    implementation(libs.com.jakewharton.timber)
+
+    // MPAndroidChart
+    implementation(libs.github.philJay.chart)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
