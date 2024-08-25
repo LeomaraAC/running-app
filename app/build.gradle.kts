0582ee7c3b05cd8ad0a86b40278a2b1ccba67ec0
Leomara AC <leomara.andrade@gmail.com>
2024-08-25 08:30:09 -0300

    Ajustando a dependencia do dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.google.dagger.hilt.android)
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
